9eaa8c8a4736163f2de6a71623478032283833ba
Jabame731 <justinetaglucop@gmail.com>
2026-04-08 19:15:10 +0800

    user register fn

--- lib/validator.ts
+++ lib/validator.ts
@@ -27,3 +27,17 @@ export const siginInFormSchema = z.object({
   email: z.email("Invalid email address"),
   password: z.string().min(6, "Password must be at least 6  characters"),
 });
+
+export const siginUpFormSchema = z
+  .object({
+    name: z.string().min(3, "Name must be at least 3 characters"),
+    email: z.email("Invalid email address"),
+    password: z.string().min(6, "Password must be at least 6  characters"),
+    confirmPassword: z
+      .string()
+      .min(6, "Confirm password must be at least 6  characters"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Password don't match",
+    path: ["confirmPassword"],
+  });
