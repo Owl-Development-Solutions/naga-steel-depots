294d05b3aff3993093ea0aee351513607ee6b694
Jabame731 <94708537+Jabame731@users.noreply.github.com>
2026-05-09 12:59:38 +0800

    Update page.tsx

--- app/(user)/place-order/page.tsx
+++ app/(user)/place-order/page.tsx
@@ -144,7 +144,7 @@ const PlaceOrderPage = async () => {
         <div>
           <Card>
             <CardContent className="p-4 gap-4 space-y-4">
-              {JSON.stringify(cart)}
+              
               <div className="flex justify-between">
                 <div>Items</div>
                 <div>{formatCurrency(cart.itemsPrice)}</div>
